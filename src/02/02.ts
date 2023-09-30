9d21d4dd1fa492efbe4ea3227f4702eb24c98f23
Artur <alimayko91@gmail.com>
2023-09-30 18:20:32 +0300

    lesson 2 : object

---
+++ src/02/02.ts
@@ -0,0 +1,57 @@
+type cityType = {
+    title: string
+    countryTitle: string
+}
+
+type addressType = {
+    streetTitle: string
+    city: cityType
+}
+
+type TechType = {
+    id: number
+    title: string
+}
+
+type StydentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: addressType
+    technologies: Array<TechType>
+}
+
+const student:StydentType = {     //присваиваем переменной student какой то создаваемый объект
+    id: 1,
+    name: "Artur",
+    age: 32,
+    isActive: false,
+    address: {
+        streetTitle: "Slepneva 15",
+        city: {
+            title: "Yaroslavl",
+            countryTitle: "Belarus",
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 3,
+            title: "CSS"
+        },
+        {
+            id: 2,
+            title: "React"
+        },
+    ]
+
+}
+
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.title)
+console.log(student.technologies[2].title)
