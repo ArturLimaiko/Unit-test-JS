09ec187883f44ca10d91d88790866b3e1193b3ce
Artur <alimayko91@gmail.com>
2023-09-30 23:01:04 +0300

    lesson 3: function.

--- src/03/03.test.ts
+++ src/03/03.test.ts
@@ -1,25 +1,25 @@
 
 
-01. Создайте тип CityType
-02. Заполните объект city , что бы тесты ниже прошли
-
-test("test should be contains 3 houses", () => {
-    expect(city.houses.length).toBe(3)
-
-    expect(city.houses[0].buildedAt).toBe(2012)
-    expect(city.houses[0].repaired).toBe(false)
-    expect(city.houses[0].address.number).toBe(100)
-    expect(city.houses[0].address.street.title).toBe("White Street")
-
-    expect(city.houses[1].buildedAt).toBe(2008)
-    expect(city.houses[1].repaired).toBe(false)
-    expect(city.houses[1].address.number).toBe(100)
-    expect(city.houses[1].address.street.title).toBe("Happy Street")
-
-    expect(city.houses[2].buildedAt).toBe(2020)
-    expect(city.houses[2].repaired).toBe(false)
-    expect(city.houses[2].address.number).toBe(101)
-    expect(city.houses[2].address.street.title).toBe("Happy Street")
-
-})
-
+// 01. Создайте тип CityType
+// 02. Заполните объект city , что бы тесты ниже прошли
+//
+// test("test should be contains 3 houses", () => {
+//     expect(city.houses.length).toBe(3)
+//
+//     expect(city.houses[0].buildedAt).toBe(2012)
+//     expect(city.houses[0].repaired).toBe(false)
+//     expect(city.houses[0].address.number).toBe(100)
+//     expect(city.houses[0].address.street.title).toBe("White Street")
+//
+//     expect(city.houses[1].buildedAt).toBe(2008)
+//     expect(city.houses[1].repaired).toBe(false)
+//     expect(city.houses[1].address.number).toBe(100)
+//     expect(city.houses[1].address.street.title).toBe("Happy Street")
+//
+//     expect(city.houses[2].buildedAt).toBe(2020)
+//     expect(city.houses[2].repaired).toBe(false)
+//     expect(city.houses[2].address.number).toBe(101)
+//     expect(city.houses[2].address.street.title).toBe("Happy Street")
+//
+// })
+//
